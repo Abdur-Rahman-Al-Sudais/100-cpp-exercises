e8c436f8f61850e52941f8dc9b38181d24550eb0
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-27 03:54:32 -0700

    Exercise 7 done

--- Exercise7.cpp
+++ Exercise7.cpp
@@ -1,2 +1,25 @@
-//Title: Reverse a String
-//Description: 	Write a program that asks the user to enter a string and then prints the reverse of that string.	
+// Title: Reverse a String
+// Description: 	Write a program that asks the user to enter a string and then prints the reverse of that string.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    cout << "Enter a string to reverse it: ";
+    getline(cin, str);
+
+    for (int i = 0; i < str.length() / 2; i++)
+    {
+        char temp1 = str[str.length() - i - 1];
+        char temp2 = str[i];
+        str[i] = temp1;
+        str[str.length() - i - 1] = temp2;
+    }
+
+    cout << str << endl;
+
+    return 0;
+}
