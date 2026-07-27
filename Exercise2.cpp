41decf64ee9caf66dc18c63f09fc6d349630dbf9
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-27 02:46:48 -0700

    Exercise 2 done

--- Exercise2.cpp
+++ Exercise2.cpp
@@ -1,2 +1,21 @@
 // Title: Sum of Two Numbers
-//Description: Write a program that asks the user to enter two numbers, and then prints the sum of those two numbers.
+// Description: Write a program that asks the user to enter two numbers, and then prints the sum of those two numbers.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num1;
+    double num2;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    cout << "Sum: " << num1 + num2 << endl;
+
+    return 0;
+}
