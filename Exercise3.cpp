4c52ee9ab8808ab6992a3373a2dc168f137340cb
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-27 02:50:05 -0700

    Exercise 3 done

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -1,3 +1,24 @@
-//Title: Even or Odd
-//Description: Write a program that asks the user to enter a number and then determines whether the number is even or odd. 
-//Print an appropriate message to the console.
+// Title: Even or Odd
+// Description: Write a program that asks the user to enter a number and then determines whether the number is even or odd.
+// Print an appropriate message to the console.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << "The number you entered is EVEN.";
+    }
+    else
+    {
+        cout << "The number you entered is ODD.";
+    }
+
+    return 0;
+}
