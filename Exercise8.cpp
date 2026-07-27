fe8ae6b34d3edc6d3eb82d3833cd92e2bb804274
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-27 05:21:02 -0700

    Exercise 8 done

--- Exercise8.cpp
+++ Exercise8.cpp
@@ -1,3 +1,30 @@
-//Title: Palindrome
-//Description: Write a program that asks the user to enter a string and then determines whether the string is a palindrome or not. 
-//A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.	
+// Title: Palindrome
+// Description: Write a program that asks the user to enter a string and then determines whether the string is a palindrome or not.
+// A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str, reversed = "";
+    cout << "Enter a string: ";
+    getline(cin, str);
+
+    for (int i = str.length() - 1; i >= 0; i--)
+    {
+        reversed += str[i];
+    }
+
+    if (str == reversed)
+    {
+        cout << "The string you entered is a Palindrome." << endl;
+    }
+    else
+    {
+        cout << "The string you entered is NOT a Palindrome." << endl;
+    }
+
+    return 0;
+}
