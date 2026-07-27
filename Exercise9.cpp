6616d82e08551aedd5180fbac0b0eaad2a185fb8
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-27 05:30:46 -0700

    Exercise 9 done

--- Exercise9.cpp
+++ Exercise9.cpp
@@ -1,2 +1,20 @@
-//Title:	Multiplication Table	
-//Description: Write a program that prints the multiplication table for numbers from 1 to 10.	
+// Title:	Multiplication Table
+// Description: Write a program that prints the multiplication table for numbers from 1 to 10.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << "Multiplication Table for " << i << endl;
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << i << " x " << j << " = " << i * j << '.' << endl;
+        }
+    }
+
+    return 0;
+}
