3aeadefcbe5018638fed9c2066d834aa4c52b5a2
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-27 03:17:41 -0700

    Exercise 5 done

--- Exercise5.cpp
+++ Exercise5.cpp
@@ -1,3 +1,24 @@
-//Title: Factorial
-//Description: Write a program that calculates the factorial of a given number.
-//The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.	
+// Title: Factorial
+// Description: Write a program that calculates the factorial of a given number.
+// The factorial of a non-negative integer n is the product of all positive integers less than or equal to n.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number to find its factorial: ";
+    cin >> num;
+
+    int factorial = 1;
+
+    for (int i = num; i > 0; i--)
+    {
+        factorial *= i;
+    }
+
+    cout << "The factorial of " << num << " is " << factorial << endl;
+
+    return 0;
+}
