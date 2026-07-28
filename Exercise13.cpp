f3b6ef9c2e0409fea52236ce317d66910b31d3cb
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-28 00:19:42 -0700

    Exercise 13 done

--- Exercise13.cpp
+++ Exercise13.cpp
@@ -1,2 +1,22 @@
-//Title:	Array Sum	
-//Description: Write a program that calculates the sum of all elements in an array. Initialize the array with some predefined values.
+// Title:	Array Sum
+// Description: Write a program that calculates the sum of all elements in an array. Initialize the array with some predefined values.
+
+#include <iostream>
+#include <iterator>
+using namespace std;
+
+int main()
+{
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+
+    size_t length = sizeof(arr) / sizeof(arr[0]);
+
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += arr[i];
+    }
+
+    cout << "Sum: " << sum << endl;
+    return 0;
+}
