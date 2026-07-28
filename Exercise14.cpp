9ccc87fd8f04db537e8ae6216117188582cea579
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-28 00:27:03 -0700

    Exercise 14 done

--- Exercise14.cpp
+++ Exercise14.cpp
@@ -1,2 +1,23 @@
-//Title: Array Average	
-//Description: Write a program that calculates the average of all elements in an array. Initialize the array with some predefined values.
+// Title: Array Average
+// Description: Write a program that calculates the average of all elements in an array. Initialize the array with some predefined values.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {30, 30, 40};
+
+    size_t length = sizeof(arr) / sizeof(arr[0]);
+
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += arr[i];
+    }
+
+    float average = static_cast<float>(sum) / length;
+
+    cout << "Average: " << average << endl;
+    return 0;
+}
