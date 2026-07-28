5355b22e8edacb7969bcba26b683c2ad9f30a16e
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-28 00:33:23 -0700

    Exercise 15 done

--- Exercise15.cpp
+++ Exercise15.cpp
@@ -1,3 +1,25 @@
-//Title: Array Sorting
-//Description: Write a program that sorts the elements of an array in ascending order. 
-//You can use any sorting algorithm of your choice (e.g., bubble sort, selection sort, insertion sort).
+// Title: Array Sorting
+// Description: Write a program that sorts the elements of an array in ascending order.
+// You can use any sorting algorithm of your choice (e.g., bubble sort, selection sort, insertion sort).
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int arr[] = {6,1,82,21,22,13,893,2000};
+
+    size_t length = sizeof(arr) / sizeof(arr[0]);
+
+    sort(arr, arr + length);
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
