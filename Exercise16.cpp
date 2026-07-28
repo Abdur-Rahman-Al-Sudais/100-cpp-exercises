06b98846124c39906a465a617ae8f8510d116861
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-28 00:38:40 -0700

    Exercise 16 done

--- Exercise16.cpp
+++ Exercise16.cpp
@@ -1,2 +1,21 @@
-//Title:	String Length	
-//Description:Write a program that asks the user to enter a string and calculates the length of the string (the number of characters in the string).
+// Title:	String Length
+// Description:Write a program that asks the user to enter a string and calculates the length of the string (the number of characters in the string).
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout << "Enter any string: ";
+
+    getline(cin, str);
+
+    size_t length = str.length();
+
+    cout << "Length: " << length << endl;
+
+    return 0;
+}
