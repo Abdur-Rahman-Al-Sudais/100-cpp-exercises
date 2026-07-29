d81b631029cb7e0823a132ffa5592d7c1e73babc
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-29 03:30:19 -0700

    Exercise 18 done

--- Exercise18.cpp
+++ Exercise18.cpp
@@ -1,2 +1,25 @@
-//Title: Sum of Digits	
-//Description: Write a program that asks the user to enter a positive integer and calculates the sum of its digits.
+// Title: Sum of Digits
+// Description: Write a program that asks the user to enter a positive integer and calculates the sum of its digits.
+
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+int main()
+{
+    string numStr;
+
+    cout << "Enter a positive integer: ";
+    cin >> numStr;
+
+    int sum = 0;
+    for (size_t i = 0; i < numStr.length(); i++)
+    {
+
+        sum += numStr[i] - '0';
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
