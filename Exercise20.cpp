518071009788a36fdd5f35d30c6110bdf0c84467
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-30 02:26:12 -0700

    Exercise 20 done

--- Exercise20.cpp
+++ Exercise20.cpp
@@ -1,3 +1,44 @@
-//Title: Palindrome Array	
-//Description: Write a program that asks the user to enter an array of integers and determines whether the array is a palindrome or not. 
-//An array is considered a palindrome if it reads the same forward and backward.
+// Title: Palindrome Array
+// Description: Write a program that asks the user to enter an array of integers and determines whether the array is a palindrome or not.
+// An array is considered a palindrome if it reads the same forward and backward.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int size;
+    bool isPalindrome = true;
+
+    cout << "Enter the number of elements: ";
+    cin >> size;
+
+    int arr[size];
+
+    cout << "Enter " << size << " integers: " << endl;
+    for (size_t i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] != arr[size - i - 1])
+        {
+            isPalindrome = false;
+            break;
+        }
+    }
+
+    if (isPalindrome)
+    {
+        cout << "Array is Palindrome.";
+    }
+    else
+    {
+        cout << "Array is not Palindrome.";
+    }
+
+    return 0;
+}
