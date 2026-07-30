271c840de4cf55bd5bf2df62d6f1a7a1f06177f6
Abdur Rahman Al Sudais <sudaiswebdev@gmail.com>
2026-07-30 02:47:15 -0700

    Exercise 21 done

--- Exercise21.cpp
+++ Exercise21.cpp
@@ -1,2 +1,48 @@
-//Title: Prime Numbers in a Range	
-//Description: Write a program that asks the user to enter a range (start and end values) and prints all prime numbers within that range.
+// Title: Prime Numbers in a Range
+// Description: Write a program that asks the user to enter a range (start and end values) and prints all prime numbers within that range.
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num)
+{
+    if (num < 2)
+        return false;
+    for (int i = 2; i * i <= num; i++)
+    {
+        if (num % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int start, end;
+
+    cout << "Enter the start of the range: ";
+    cin >> start;
+    cout << "Enter the end of the range: ";
+    cin >> end;
+
+    int range = end - start;
+
+    if (range <= 0)
+    {
+        cout << "Invalid range.";
+        return 0;
+    }
+
+    cout << "Prime numbers between " << start << " and " << end << ":" << endl;
+    for (int i = start; i <= end; i++)
+    {
+        if (isPrime(i))
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
